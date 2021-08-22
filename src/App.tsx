f12629ce59c7b528c432b13397145980bec2e0fb
uniquejava <uniquejava@gmail.com>
2021-08-22 19:29:35 +0800

    font tts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import logo from "./assets/images/logo.svg";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
@@ -5,6 +6,10 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map((r) => (
           <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
