9b9c04cc78e4627b6412ee1bf6ded9b12cf329b1
uniquejava <uniquejava@gmail.com>
2021-08-25 16:20:33 +0800

    useEffect async loading and exception

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
 import logo from "./assets/images/logo.svg";
-import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
 import ShoppingCart from "./components/ShoppingCart";
-function App() {
+import { useEffect, useState } from "react";
+const App: React.FC = (props) => {
+  const [count, setCount] = useState(0);
+  const [robotGallery, setRobotGallery] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    document.title = `点击了${count}次`;
+  }, [count]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        setRobotGallery(data);
+      } catch (err) {
+        setError(err.message);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.app}>
       <div className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt="logo" />
         <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
       </div>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+      <span>count: {count}</span>
+      {error && <div>网站出错: {error} </div>}
       <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((r) => (
-          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
-        ))}
-      </div>
+      {!loading ? (
+        <div className={styles.robotList}>
+          {robotGallery.map((r) => (
+            <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
+          ))}
+        </div>
+      ) : (
+        <h2>loading加载中 </h2>
+      )}
     </div>
   );
-}
+};
 
 export default App;
