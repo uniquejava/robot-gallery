984c431fdd8cecb4522dab94277eecfb6981c734
uniquejava <uniquejava@gmail.com>
2021-08-25 14:37:09 +0800

    context api

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Robot.module.css";
-
+import { appContext } from "../index";
 interface RobotProps {
   id: number;
   name: string;
   email: string;
 }
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  const value = useContext(appContext);
   return (
     <div className={styles.cardContainer}>
       <img alt="robot" src={"https://robohash.org/" + id} />
       <div>{name}</div>
       <div>{email}</div>
+      <div>作者：{value.username}</div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const defaultContextValue = {
+  username: "阿西莱福特",
+};
+
+export const appContext = React.createContext(defaultContextValue);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <appContext.Provider value={defaultContextValue}>
+      <App />
+    </appContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
